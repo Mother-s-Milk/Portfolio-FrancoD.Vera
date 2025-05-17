908f189b79eae2aff24a63bea5022bef806514a8
Franco Vera <79548455+Mother-s-Milk@users.noreply.github.com>
2025-05-16 22:50:09 -0300

    Update mainService.js

--- public/app/js/mainService.js
+++ public/app/js/mainService.js
@@ -1,6 +1,6 @@
 export const mainService = {
     obtenerProyectos: () => {
-        return fetch ('https://github.com/Mother-s-Milk/Portfolio-FrancoD.Vera/database/proyectos.json', {
+        return fetch ('https://mother-s-milk.github.io/Portfolio-FrancoD.Vera/database/proyectos.json', {
             method: "GET",
             headers: {}
         })
