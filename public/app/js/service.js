2b1fb3ae859b23e29447be9d5f82ae1df9e2c9f3
Mother-s-Milk <francoveradrms@gmail.com>
2025-09-25 16:48:27 -0300

    Se suben algunas actualizaciones

--- public/app/js/service.js
+++ public/app/js/service.js
@@ -1,7 +1,7 @@
 export const service = {
     getProjects: () => {
-        //return fetch ('https://mother-s-milk.github.io/Portfolio-FrancoD.Vera/database/projects.json', {
-        return fetch ('../../../database/projects.json', {
+        return fetch ('https://mother-s-milk.github.io/Portfolio-FrancoD.Vera/database/projects.json', {
+        //return fetch ('../../../database/projects.json', {
             method: "GET",
             headers: {}
         })
